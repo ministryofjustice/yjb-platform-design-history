ec08ba91a061f09339226d541696efd0ee001097
Owen Blacker <owen.blacker@yjb.gov.uk>
2026-06-11 13:48:22 +0100

    Set path prefix for Github Actions

--- eleventy.config.js
+++ eleventy.config.js
@@ -55,6 +55,6 @@ export default function (eleventyConfig) {
       layouts: '_layouts',
       includes: '_components'
     },
-    // pathPrefix: process.env.GITHUB_ACTIONS && '/govuk-design-history-template/'
+    pathPrefix: process.env.GITHUB_ACTIONS ? '/yjb-platform-design-history/' : '/'
   }
 }
